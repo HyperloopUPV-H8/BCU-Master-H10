257eb31215b23c763e6fbb07de87d92c1f548144
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2024-12-01 23:36:53 +0100

    fix cmake for VSCode and simple CAN sender

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -6,10 +6,23 @@
 
 #include "BCU/BCU.hpp"
 
+using enum CMS::DataRequestFlags;
+bool transmit_can = false;
 int main(void) {
     BCU::BCU bcu = BCU::BCU();
-
+    //BCU::Communication::FDCAN::request_all_data(EnableAutoTx | SendCellsVoltage,1000);
+    char data[8] = {
+    0x55,0x55,0x55,0x55,
+    0x55,0x55,0x55,0x55
+    };
+    Time::register_low_precision_alarm(1000,[&](){
+        transmit_can = true;
+    });
     while (1) {
+        if(transmit_can){
+            transmit_can = false;
+            FDCAN::transmit(0,85,&data[0],FDCAN::BYTES_8);
+        }
         bcu.update();
     }
 }
