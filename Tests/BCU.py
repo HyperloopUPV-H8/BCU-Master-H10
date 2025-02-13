5789b3c2b7247f7701fa302412801716c41db248
Cantonplas <dcancat@etsinf.upv.es>
2025-02-13 15:50:14 +0100

    Temporal test finished

--- Tests/BCU.py
+++ Tests/BCU.py
@@ -5,7 +5,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "VirtualMCU", "src"))
 import src.vmcu.assertions as assertions
 import src.vmcu.services.communications.FDCAN as FDCAN
 import src.vmcu.pin.pinout as Pinout
-import src.vmcu.services.digital_in as DigitalInService
 import src.vmcu.services.digital_out as DigitalOutService
 import src.vmcu.shared_memory as SharedMemory
 import src.vmcu.services.communications.spi as SPI
@@ -20,19 +19,22 @@ class BCU():
         OPERATIONAL = 1
         FAULT = 2
         
-    def __init__(self):
-        self.shm = SharedMemory("gpio__blinking__led","state_machine__blinking__led")
-        #TODO: create __init__
-        
+    def __init__(self,high:Pinout,precharge: Pinout,d_sup: Pinout):
+        self.shm = SharedMemory("gpio_BCU","state_machine_BCU")
+        self.contactor = self.ContactorTest(high,precharge,d_sup)
+        self.CAN = self.FDCAN_test(self.shm, Pinout.PB11, Pinout.PB10)
+        self.GUI = self.Ethernet_tests("localhost", 50400, "localhost", 50500)
+
         
     def is_state(self,state:int):
-            current_state = self.shm.get_state_machine_state(1)
+            current_state = self.shm.get_state_machine_state(0)
             return current_state == state
+        
     class ContactorTest:
         def __init__(self,high:Pinout,precharge: Pinout,d_sup: Pinout):
             self.high = DigitalOutService(self.shm, high)
             self.precharge = DigitalOutService(self.shm, precharge)
-            self.d_sup = DigitalInService(self.shm, d_sup)
+            self.d_sup = DigitalOutService(self.shm, d_sup)
             
         def check_charging(self):
             def check_charging_contactors():
@@ -43,7 +45,7 @@ class BCU():
                 if self.d_sup.get_pin_state() == 1:
                     return False
                 return True
-            assertions.completes(assertions.wait_until_true(check_charging_contactors), before=assertions.seconds(1), msg="Contactors are not closed while in charging state")
+            assertions.check(check_charging_contactors, msg="Contactors are not closed while in charging state")
         
         def check_open(self):
             def check_open_contactors():
@@ -54,7 +56,7 @@ class BCU():
                 if self.d_sup.get_pin_state() == 0:
                     return False
                 return True
-            assertions.completes(assertions.wait_until_true(check_open_contactors), before=assertions.seconds(1), msg="Contactors are not open while in open state")
+            assertions.check(check_open_contactors, msg="Contactors are not open while in open state")
             
         
         def check_close(self):
@@ -66,7 +68,7 @@ class BCU():
                 if self.d_sup.get_pin_state() == 1:
                     return False
                 return True
-            assertions.completes(assertions.wait_until_true(check_close_contactors), before=assertions.seconds(1), msg="Contactors are not closed while in close state")
+            assertions.check(check_close_contactors,msg="Contactors are not closed while in close state")
         
     class FDCAN_test:
         def __init__(self,shm:SharedMemory, Rx: Pinout, Tx: Pinout):
@@ -87,23 +89,24 @@ class BCU():
         def start(self, ip: str, port: int,sendport:int):
             self.fdcan.start(ip, port,sendport)
 
-        def receivettest(self):
+        def receive_test(self):
             def PacketReceived():
                 packet = self.fdcan.read()
                 if packet != None: #TODO: packet not defined
                     return True
-                
+                return False
             assertions.completes(assertions.wait_until_true(PacketReceived), before=assertions.seconds(1), msg="FDCAN did not receive the expected packet")
-        def testSupercaps(self):
+            
+        def send_data_supercaps(self):
             self.receivettest()
             for i in {0,2}:
                 for j in {1,4}:
-                    assertions.completes(self.fdcan.transmit(i,self.fdcanData[i][j],FDCAN.DLC.BYTES_8), before=assertions.seconds(1), msg="FDCAN did not transmit")
+                    assertions.completes(assertions.wait_until_true(self.fdcan.transmit(i,self.fdcanData[i][j],FDCAN.DLC.BYTES_8)), before=assertions.seconds(1), msg="FDCAN did not transmit")
         
-        def testresetSupercaps(self):
+        def reset_supercaps(self):
             self.receivettest()
             for i in {0,2}:
-                assertions.completes(self.fdcan.transmit(i,self.fdcanData[i][7],FDCAN.DLC.BYTES_8), before=assertions.seconds(1), msg="FDCAN did not transmit")
+                assertions.completes(assertions.wait_until_true(self.fdcan.transmit(i,self.fdcanData[i][7],FDCAN.DLC.BYTES_8)), before=assertions.seconds(1), msg="FDCAN did not transmit")
             
     class SPI_tests:#TODO
         def __init__(self):
@@ -125,21 +128,25 @@ class BCU():
         def disconnect_gui(self):
             self.sock.stop()
             
-        def transmit_opencontactors(self):
+        def transmit_open_contactors(self)->bool:
             self.sock.transmit(self._opencontactors_Packet)
             return self.sock.is_running()
 
-        def transmit_closecontactors(self):
+        def transmit_close_contactors(self)->bool:
             self.sock.transmit(self._closecontactors_Packet)
             return self.sock.is_running()
         
-        def check_received_packet(self)->bool:
-            raw_data = self.sock.get_packet()
-            if raw_data is None: 
-                return False
-            return True
+        def check_packet(self)->bool: #def check_packet(self,id,value)->bool:
+            def check_received_packet():#def check_received_packet(id,value):
+                raw_data = self.sock.get_packet()
+                if raw_data is None: 
+                    return False
             #id,value = struct.unpack("<Hi",raw_data) used in test from template_project
             #return id == 1 and value == i + 5
+
+                return True
+            assertions.completes(assertions.wait_until_true(check_received_packet), before=assertions.seconds(1), msg="Packet not received")
+           
         
         def __del__(self):
             self.sock.stop()
--- Tests/test.py
+++ Tests/test.py
@@ -2,49 +2,52 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "VirtualMCU", "src"))
 
 from runner import runner
-from vmcu.shared_memory import SharedMemory
-from vmcu.pin import Pinout, DigitalOut
-from vmcu.services.digital_out import DigitalOutService
-from vmcu.assertions import *
+from src.vmcu.shared_memory import SharedMemory
+from src.vmcu.pin import Pinout, DigitalOut
+from src.vmcu.services.digital_out import DigitalOutService
+import src.vmcu.assertions as assertions
+import BCU as BCU
+import time
 
 
 @runner.test()
-def led_toggle():
-    TOGGLE_PERIOD = milliseconds(100 * 2)
-    ALLOWED_SLACK = milliseconds(5)
-
-    shm = SharedMemory("gpio__blinking_led", "state_machine__blinking_led")
-    led = DigitalOutService(shm, Pinout.PA1)
-
-    def led_turns_on():
-        nonlocal led
-        return led.get_pin_state() is DigitalOut.State.High
-
-    def led_turns_off():
-        nonlocal led
-        return led.get_pin_state() is DigitalOut.State.Low
-
-    #sync with board
-    completes(
-        wait_until_true(led_turns_on),
-        before=(TOGGLE_PERIOD / 2) + ALLOWED_SLACK,
-        msg="Sync fails"
-    )
-
-    for i in range(150):
-        completes(
-            wait_until_true(led_turns_off),
-            before=(TOGGLE_PERIOD / 2) + ALLOWED_SLACK,
-            after=(TOGGLE_PERIOD / 2) - ALLOWED_SLACK,
-            msg="turns off"
-        )
-        completes(
-            wait_until_true(led_turns_on),
-            before=(TOGGLE_PERIOD / 2) + ALLOWED_SLACK,
-            after=(TOGGLE_PERIOD / 2) - ALLOWED_SLACK,
-            msg="turns on"
-        )
-        print("toggle", i)
+def BCU_test():
+    #Initialize the BCU test class
+    BCU = BCU(Pinout.PA1, Pinout.PA2, Pinout.PA3)
+    
+    #BCU is currently in connecting state:
+    assertions.check(BCU.is_state,args=(BCU.General_SM.CONNECTING), msg="BCU is not in connecting state")
+    BCU.contactor.check_charging()
+    #Start recieving data from the supercaps CAN:
+    BCU.CAN.start()
+    BCU.CAN.receive_test()
+    time.sleep(0.5)
+    BCU.CAN.send_data_supercaps()
+    
+    
+    #BCU transition to operational:
+    BCU.GUI.connect_gui()
+    time.sleep(0.5)
+    assertions.check(BCU.is_state,args=(BCU.General_SM.OPERATIONAL), msg="BCU is not in operational state")
+    
+    #BCU operational:
+    
+    #Open contactors:
+    BCU.GUI.transmit_open_contactors()
+    time.sleep(0.5)
+    BCU.contactor.check_open()
+    
+    #Close contactors:
+    BCU.GUI.transmit_close_contactors()
+    time.sleep(0.5)
+    BCU.contactor.check_charging()
+    
+    #BCU transition to fault:
+    BCU.CAN.disconnect_gui()
+    time.sleep(0.5)
+    assertions.check(BCU.is_state,args=(BCU.General_SM.FAULT), msg="BCU is not in fault state")
+    
+    
     
 
 runner.run() # Runs the tests, do not delete!
