77a6d8d298a13de54c7bb940689867d048cd90a7
Catonplas <dcancat@etsinf.upv.es>
2025-02-13 17:33:16 +0100

    Merge branch 'Python_tests' of https://github.com/HyperloopUPV-H8/BCU-Master-H10 into Python_tests

--- Tests/BCU.py
+++ Tests/BCU.py
@@ -25,50 +25,60 @@ class BCU():
         self.CAN = self.FDCAN_test(self.shm, Pinout.PB11, Pinout.PB10)
         self.GUI = self.Ethernet_tests("localhost", 50400, "localhost", 50500)
 
+    def __init__(self,high:Pinout,precharge: Pinout,d_sup: Pinout):
+        self.shm = SharedMemory("gpio_BCU","state_machine_BCU")
+        self.contactor = self.ContactorTest(high,precharge,d_sup)
+        self.CAN = self.FDCAN_test(self.shm, Pinout.PB11, Pinout.PB10)
+        self.GUI = self.Ethernet_tests("localhost", 50400, "localhost", 50500)
+
         
     def is_state(self,state:int):
             current_state = self.shm.get_state_machine_state(0)
+            current_state = self.shm.get_state_machine_state(0)
             return current_state == state
+        
         
     class ContactorTest:
         def __init__(self,high:Pinout,precharge: Pinout,d_sup: Pinout):
             self.high = DigitalOutService(self.shm, high)
             self.precharge = DigitalOutService(self.shm, precharge)
             self.d_sup = DigitalOutService(self.shm, d_sup)
-            
-        def check_charging(self):
-            def check_charging_contactors():
-                if self.high.get_pin_state() == 1:
-                    return False
-                if self.precharge.get_pin_state() == 1:
-                    return False
-                if self.d_sup.get_pin_state() == 1:
-                    return False
-                return True
-            assertions.check(check_charging_contactors, msg="Contactors are not closed while in charging state")
+        
+        def check_open_contactors(self):
+            return self.high.get_pin_state() == 0 and self.precharge.get_pin_state() == 0 and self.d_sup.get_pin_state() == 0
+        
+        def check_precharging_contactors(self):
+            return self.high.get_pin_state() == 0 and self.precharge.get_pin_state() == 1 and self.d_sup.get_pin_state() == 1
+        
+        def check_charging_contactors(self):
+            return self.high.get_pin_state() == 0 and self.precharge.get_pin_state() == 0 and self.d_sup.get_pin_state() == 1
+        
+        def check_close_contactors(self):
+            return self.high.get_pin_state() == 1 and self.precharge.get_pin_state() == 0 and self.d_sup.get_pin_state() == 1
         
         def check_open(self):
-            def check_open_contactors():
-                if self.high.get_pin_state() == 1:
-                    return False
-                if self.precharge.get_pin_state() == 1:
-                    return False
-                if self.d_sup.get_pin_state() == 0:
-                    return False
-                return True
-            assertions.check(check_open_contactors, msg="Contactors are not open while in open state")
-            
+            assertions.check(self.check_open_contactors, msg="Contactors are not closed while in open state")
+        
+        def completes_open(self,before: float|None=None,after:float|None=None):
+            assertions.completes(assertions.wait_until_true(self.check_open_contactors), before=before,after=after, msg="Contactors are not closed while in open state")
+        
+        def check_precharging(self):
+            assertions.check(self.check_precharging_contactors, msg="Contactors are not open while in discharging state")
+        
+        def completes_precharging(self,before: float|None=None,after:float|None=None):
+            assertions.completes(assertions.wait_until_true(self.check_precharging_contactors), before=before,after=after, msg="Contactors are not open while in discharging state")
         
         def check_close(self):
-            def check_close_contactors():
-                if self.high.get_pin_state() == 0:
-                    return False
-                if self.precharge.get_pin_state() == 0:
-                    return False
-                if self.d_sup.get_pin_state() == 1:
-                    return False
-                return True
-            assertions.check(check_close_contactors,msg="Contactors are not closed while in close state")
+            assertions.check(self.check_close_contactors,msg="Contactors are not closed while in close state")
+        
+        def completes_close(self,before: float|None=None,after:float|None=None):
+            assertions.completes(assertions.wait_until_true(self.check_close_contactors), before=before,after=after, msg="Contactors are not closed while in close state")
+        
+        def check_charging(self):
+            assertions.check(self.check_charging_contactors,msg="Contactors are not open while in charging state")
+        
+        def completes_charging(self,before: float|None=None,after:float|None=None):
+            assertions.completes(assertions.wait_until_true(self.check_charging_contactors), before=before,after=after, msg="Contactors are not open while in charging state")
         
     class FDCAN_test:
         def __init__(self,shm:SharedMemory, Rx: Pinout, Tx: Pinout):
@@ -90,22 +100,29 @@ class BCU():
             self.fdcan.start(ip, port,sendport)
 
         def receive_test(self):
+        def receive_test(self):
             def PacketReceived():
                 packet = self.fdcan.read()
                 if packet != None: #TODO: packet not defined
                     return True
                 return False
+                return False
             assertions.completes(assertions.wait_until_true(PacketReceived), before=assertions.seconds(1), msg="FDCAN did not receive the expected packet")
+            
+        def send_data_supercaps(self):
             
         def send_data_supercaps(self):
             self.receivettest()
             for i in {0,2}:
                 for j in {1,4}:
                     assertions.completes(assertions.wait_until_true(self.fdcan.transmit(i,self.fdcanData[i][j],FDCAN.DLC.BYTES_8)), before=assertions.seconds(1), msg="FDCAN did not transmit")
+                    assertions.completes(assertions.wait_until_true(self.fdcan.transmit(i,self.fdcanData[i][j],FDCAN.DLC.BYTES_8)), before=assertions.seconds(1), msg="FDCAN did not transmit")
         
+        def reset_supercaps(self):
         def reset_supercaps(self):
             self.receivettest()
             for i in {0,2}:
+                assertions.completes(assertions.wait_until_true(self.fdcan.transmit(i,self.fdcanData[i][7],FDCAN.DLC.BYTES_8)), before=assertions.seconds(1), msg="FDCAN did not transmit")
                 assertions.completes(assertions.wait_until_true(self.fdcan.transmit(i,self.fdcanData[i][7],FDCAN.DLC.BYTES_8)), before=assertions.seconds(1), msg="FDCAN did not transmit")
             
     class SPI_tests:#TODO
@@ -115,8 +132,8 @@ class BCU():
         def __init__(self,lip, lport, rip, rport):
             self.sock = Socket(lip, lport, rip, rport)
             packet_definition= {
-                900:bytearray([0x03,0x84,...]),
-                901:bytearray([0x03,0x85,...])
+                900:[],
+                901:[]
             }
             packets= Packets(packet_definition)
             self._opencontactors_Packet= packets.serialize_packets(900)
@@ -129,9 +146,11 @@ class BCU():
             self.sock.stop()
             
         def transmit_open_contactors(self)->bool:
+        def transmit_open_contactors(self)->bool:
             self.sock.transmit(self._opencontactors_Packet)
             return self.sock.is_running()
 
+        def transmit_close_contactors(self)->bool:
         def transmit_close_contactors(self)->bool:
             self.sock.transmit(self._closecontactors_Packet)
             return self.sock.is_running()
@@ -141,8 +160,17 @@ class BCU():
                 raw_data = self.sock.get_packet()
                 if raw_data is None: 
                     return False
+        def check_packet(self)->bool: #def check_packet(self,id,value)->bool:
+            def check_received_packet():#def check_received_packet(id,value):
+                raw_data = self.sock.get_packet()
+                if raw_data is None: 
+                    return False
             #id,value = struct.unpack("<Hi",raw_data) used in test from template_project
             #return id == 1 and value == i + 5
+
+                return True
+            assertions.completes(assertions.wait_until_true(check_received_packet), before=assertions.seconds(1), msg="Packet not received")
+           
 
                 return True
             assertions.completes(assertions.wait_until_true(check_received_packet), before=assertions.seconds(1), msg="Packet not received")
--- Tests/test.py
+++ Tests/test.py
@@ -9,38 +9,37 @@ import src.vmcu.assertions as assertions
 import BCU as BCU
 import time
 
+    
 
 @runner.test()
-def BCU_test():
+def contactors_close_test():
     #Initialize the BCU test class
     BCU = BCU(Pinout.PA1, Pinout.PA2, Pinout.PA3)
     
     #BCU is currently in connecting state:
     assertions.check(BCU.is_state,args=(BCU.General_SM.CONNECTING), msg="BCU is not in connecting state")
-    BCU.contactor.check_charging()
+    BCU.contactor.check_open()
     #Start recieving data from the supercaps CAN:
-    BCU.CAN.start()
-    BCU.CAN.receive_test()
-    time.sleep(0.5)
-    BCU.CAN.send_data_supercaps()
+    #BCU.CAN.start()
+    #BCU.CAN.receive_test()
+    #BCU.CAN.send_data_supercaps()
     
     
     #BCU transition to operational:
     BCU.GUI.connect_gui()
-    time.sleep(0.5)
-    assertions.check(BCU.is_state,args=(BCU.General_SM.OPERATIONAL), msg="BCU is not in operational state")
+    assertions.completes(assertions.wait_until_true(BCU.is_state),args=(BCU.General_SM.OPERATIONAL),before=(assertions.milliseconds(500)), msg="BCU is not in operational state")
+    BCU.contactor.check_open()
     
     #BCU operational:
     
     #Open contactors:
-    BCU.GUI.transmit_open_contactors()
-    time.sleep(0.5)
-    BCU.contactor.check_open()
+    BCU.GUI.transmit_close_contactors()
+    BCU.contactor.completes_precharging(before = assertions.milliseconds(500))
+    BCU.contactor.completes_close(before = assertions.seconds(5.5),after= assertions.seconds(5))
     
     #Close contactors:
-    BCU.GUI.transmit_close_contactors()
-    time.sleep(0.5)
-    BCU.contactor.check_charging()
+    BCU.GUI.transmit_open_contactors()
+    BCU.contactor.completes_open(before = assertions.milliseconds(500))
     
     #BCU transition to fault:
     BCU.CAN.disconnect_gui()
